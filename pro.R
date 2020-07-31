f8d8046faf058e8b3bfc4d1ae89fc99653ffb372
negikartik <42437949+negikartik@users.noreply.github.com>
2020-07-31 11:09:37 +0530

    Add files via upload

---
+++ pro.R
@@ -0,0 +1,15 @@
+vec1=c(1,2,3)
+vec2=c("c","c++","java")
+print(df)
+print(df[1:2,])
+weight=c(23,50,70)
+df=data.frame(vec1,vec2,weight)
+pd
+pd
+fd=subset(df,vec1==1|weight<50)
+fd
+df=rbind(df,data.frame(v4=4,v5="python",v6=69))
+df
+df
+df
+df
